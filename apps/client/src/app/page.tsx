b794cee5159c52e17ebdc86e976ec173bcc827a5
Fhonzzy <donchibs59@gmail.com>
2026-08-27 13:57:23 +0100

    Homepage development (unfinished)

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,6 +1,12 @@
+import ProductList from "@/components/ProductList"
+import Image from "next/image"
+
 const Homepage = () => {
   return <div className="">
-    Homepage
+    <div className="relative aspect-[3/1] mb-12">
+      <Image src={"/featured.png"} alt="Featured Product" fill loading="eager"/>
+    </div>
+    <ProductList />
   </div>
 }
 
