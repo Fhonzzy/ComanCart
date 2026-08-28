a048371b88304777a1054ac416900fc63edc960b
Fhonzzy <donchibs59@gmail.com>
2026-08-28 17:53:43 +0100

    Product Listing page still in progress. Encounted a bug when mapping over product sizes

--- apps/client/src/components/ProductCard.tsx
+++ apps/client/src/components/ProductCard.tsx
@@ -1,19 +1,45 @@
-"use client"
+"use client";
 
-import { ProductType } from "@/Types/type"
-import Image from "next/image"
-import Link from "next/link"
+import { ProductType } from "@/Types/type";
+import Image from "next/image";
+import Link from "next/link";
 
-const ProductCard = ({product}: {product: ProductType}) => {
+const ProductCard = ({ product }: { product: ProductType }) => {
   return (
     <div className="shadow-lg rounded-lg overflow-hidden">
+      {/*PRODUCT IMAGE*/}
       <Link href={`/products/${product.id}`}>
         <div className="relative aspect-[2/3]">
-          <Image src={product.images[product.colors[0]]} alt={product.name} fill className="object-cover hover:scale-105 transition-all duration-300" sizes=""/>
+          <Image
+            src={product.images[product.colors[0]]}
+            alt={product.name}
+            fill
+            className="object-cover hover:scale-105 transition-all duration-300"
+            sizes=""
+          />
         </div>
       </Link>
+      {/*PRODUCT DETAILS*/}
+      <div className="flex flex-col gap-4 p-4">
+        <h1 className="font-medium">{product.name}</h1>
+        <p className="text-sm text-gray-500">{product.shortDescription}</p>
+        {/*PRODUCT TYPES*/}
+        <div className="flex items-center gap-4 text-xs">
+          {/*PRODUCT SIZES*/}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Size</span>
+            <select name="size" id="size" className="ring ring-gray-300 rounded-md px-2">
+              {product.sizes.map(size => (
+                <option value={size} key={size}>{size.toUpperCase()}</option>
+              ))}
+            </select>
+          </div>
+          {/*PRODUCT COLORS*/}
+          <div></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
