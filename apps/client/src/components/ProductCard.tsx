6db993e05aaaf59e538c210b7d0592dd227fd1ef
Fhonzzy <donchibs59@gmail.com>
2026-09-08 11:46:48 +0100

    Implemented Zustand as a state management tool to persist and mange data in the cart function

--- apps/client/src/components/ProductCard.tsx
+++ apps/client/src/components/ProductCard.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import useCartStore from "@/store/cartStore";
 import { ProductType } from "@/Types/type";
 import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
   const [productTypes, setProductTypes] = useState({
     color: product.colors[0],
     size: product.sizes[0],
   });
+
+  const {addToCart} = useCartStore()
 
   const handleProductType = ({ type, value,}: {
     type: "size" | "color";
@@ -21,6 +25,17 @@ const ProductCard = ({ product }: { product: ProductType }) => {
       [type]: value,
     }));
   };
+
+  const handleAddToCart = () => {
+    addToCart({
+      ...product,
+      quantity: 1,
+      selectedSize: productTypes.size,
+      selectedColor: productTypes.color
+    })
+    toast.success("Product added to cart")
+  }
+
   return (
     <div className="shadow-lg rounded-lg overflow-hidden">
       {/*PRODUCT IMAGE*/}
@@ -83,7 +98,7 @@ const ProductCard = ({ product }: { product: ProductType }) => {
         {/*PRICE AND ATC BUTTON*/}
         <div className="flex items-center justify-between">
           <p className="font-medium">${product.price.toFixed(2)}</p>
-          <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
+          <button onClick={handleAddToCart} className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
             <ShoppingCart className="w-4 h-4" />
             Add to Cart
           </button>
